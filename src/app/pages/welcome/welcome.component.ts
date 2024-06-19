fd51fe4229192e6221f66ea2510d30343bbf30ec
sergei demin <s.demin@x5.ru>
2024-06-19 17:23:42 +0300

    init

---
+++ src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {NzTableComponent, NzTableModule} from "ng-zorro-antd/table";
+import {TableComponent} from "../../components/table.component";
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  templateUrl: './welcome.component.html',
+  imports: [
+    NzTableComponent,
+    NzTableModule,
+    TableComponent
+  ],
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
