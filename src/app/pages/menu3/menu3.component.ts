2651b1ccc43ed4019b6398ef60d0aa9de34e72e4
sergei demin <s.demin@x5.ru>
2024-07-21 00:04:49 +0300

    init

--- src/app/pages/menu3/menu3.component.ts
+++ src/app/pages/menu3/menu3.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {NzDemoTreeViewDirectoryComponent} from "../../components/tree/tree.component";
 
 @Component({
   selector: 'app-form',
   standalone: true,
   templateUrl: './menu3.component.html',
   imports: [
+    NzDemoTreeViewDirectoryComponent
   ],
   styleUrls: ['./menu3.component.css']
 })
