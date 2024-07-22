cbfc725a3be59fecdb658baf42df50f47d5d4cfc
sergei demin <s.demin@x5.ru>
2024-07-22 10:30:42 +0300

    menu style

--- src/app/pages/menu3/menu3.component.ts
+++ src/app/pages/menu3/menu3.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import {NzDemoTreeViewDirectoryComponent} from "../../components/tree/tree.component";
+import {TableComponent} from "../../components/table/table.component";
+import {NzContentComponent, NzLayoutComponent, NzSiderComponent} from "ng-zorro-antd/layout";
+import {NzFlexDirective} from "ng-zorro-antd/flex";
+import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
+import {NzTableComponent} from "ng-zorro-antd/table";
+import {NzDividerComponent} from "ng-zorro-antd/divider";
 
 @Component({
   selector: 'app-form',
   standalone: true,
   templateUrl: './menu3.component.html',
   imports: [
-    NzDemoTreeViewDirectoryComponent
+    NzDemoTreeViewDirectoryComponent,
+    TableComponent,
+    NzLayoutComponent,
+    NzSiderComponent,
+    NzContentComponent,
+    NzFlexDirective,
+    NzRowDirective,
+    NzColDirective,
+    NzTableComponent,
+    NzDividerComponent
   ],
   styleUrls: ['./menu3.component.css']
 })
