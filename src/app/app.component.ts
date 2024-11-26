64906f4fdd4a0a80a46df5d75ee3ab070c5f6e81
Demin, Sergey <s.demin@x5.ru>
2024-11-26 09:54:10 +0300

    deps up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,46 +15,43 @@ import {Menu} from "./dto/menu";
 import {NzMessageService} from "ng-zorro-antd/message";
 
 @Component({
-    selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, RouterLink, LoginComponent, ReactiveFormsModule],
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, RouterLink, LoginComponent, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-    isCollapsed = false;
-    menuList: Menu[] = [];
-    authService: AuthService;
+  isCollapsed = false;
+  menuList: Menu[] = [];
+  authService: AuthService;
 
-    constructor(authService: AuthService, http: HttpClient, private router: Router, private message: NzMessageService) {
-        this.authService = authService;
-        if (typeof window !== 'undefined') {
-            http.get<Menu[]>(environment.API_URL + '/meta/menu')
-                .subscribe({
-                    next: (menuList) => {
-                        this.menuList = menuList;
-                        menuList.forEach((menu) => {
-                            menu.routes.forEach((route) => {
-                                router.config.push({
-                                    path: route.path,
-                                    component: BaseComponent,
-                                    canActivate: [AuthGuard],
-                                    data: {metaUrl: route.metaUrl}
-                                })
-                            });
-                        });
-                    }, error: (error) => {
-                        console.error(error);
-                        this.message.create('error', `Error: ${error.message}`);
-                    }
-                });
-        }
-
+  constructor(authService: AuthService, http: HttpClient, private router: Router, private message: NzMessageService) {
+    this.authService = authService;
+    if (typeof window !== 'undefined') {
+      http.get<Menu[]>(environment.API_URL + '/meta/menu')
+        .subscribe({
+          next: (menuList) => {
+            this.menuList = menuList;
+            menuList.forEach((menu) => {
+              menu.routes.forEach((route) => {
+                router.config.push({
+                  path: route.path, component: BaseComponent, canActivate: [AuthGuard], data: {metaUrl: route.metaUrl}
+                })
+              });
+            });
+          }, error: (error) => {
+            console.error(error);
+            this.message.create('error', `Error: ${error.message}`);
+          }
+        });
     }
 
-    logout() {
-        this.authService.logout();
-        this.router.navigate(['/']);
-    }
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 
 }
