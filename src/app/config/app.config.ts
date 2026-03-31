335c69c1c40860ba0adf6a8b83273e03295c9812
S.Demin <S.Demin@x5.ru>
2026-03-31 10:03:49 +0200

    fix deprecated

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -8,7 +8,6 @@ import {en_US, provideNzI18n} from 'ng-zorro-antd/i18n';
 import {DatePipe, registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
 import {FormsModule} from '@angular/forms';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi} from '@angular/common/http';
 import {JwtInterceptor} from "../auth/jwt.Interceptor";
 import {AppLoaderService} from "../service/app.loader.service";
@@ -16,7 +15,7 @@ import {AppLoaderService} from "../service/app.loader.service";
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideNzIcons(), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(withInterceptorsFromDi(), withFetch()), {
+  providers: [provideRouter(routes), provideClientHydration(), provideNzIcons(), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideHttpClient(withInterceptorsFromDi(), withFetch()), {
     provide: HTTP_INTERCEPTORS,
     useClass: JwtInterceptor,
     multi: true
